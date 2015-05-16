e9296dee47eb82f0a09ddd91a407ffc92cdf3352
IMAZU Asami <neko.daisuki.070109@gmail.com>
2015-05-16 16:40:20 +0900

    bin/rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150516072245) do
+ActiveRecord::Schema.define(version: 20150516073246) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150516072245) do
     t.string   "item_name"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "category_id"
   end
 
 end
